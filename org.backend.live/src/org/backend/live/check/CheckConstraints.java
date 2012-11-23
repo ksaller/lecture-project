cc8f86b0cf9fbbf663d8f570f4bdf39da9b51c59
Sven Peldszus <sven.peldszus@googlemail.com>
2012-11-23 15:57:45 +0100

    Initial

--- org.backend.live/src/org/backend/live/check/CheckConstraints.java
+++ org.backend.live/src/org/backend/live/check/CheckConstraints.java
@@ -3,11 +3,12 @@ package org.backend.live.check;
 import java.util.List;
 
 import org.eclipse.emf.common.util.EList;
-import org.eclipse.emf.ecore.EObject;
 import org.emftext.term.propositional.expression.Term;
 
+import org.featuremapper.models.feature.Constraint;
 import org.featuremapper.models.feature.Feature;
 import org.backend.storage.StorageController;
+import org.west.es.context.contextModel.ClassifiedFeature;
 
 public class CheckConstraints {
 
@@ -15,19 +16,20 @@ public class CheckConstraints {
 	
 	private List<Term> terms;
 	
+	private List<ClassifiedFeature> classiefiedFeatures;
+	
 	public CheckConstraints(StorageController controller){
 		this.controller=controller;
 		terms=controller.getTerms();
 	}
 	
-	public boolean checkNewFeature(Feature feature){
-		String name = feature.getName();
-		for(Term term : terms){
-			EList<EObject> list = term.eContents();
-			for(EObject ob : list){
-				System.out.println(ob);
-			}
-		}
-		return false;
+	public boolean checkNewFeature(ClassifiedFeature classiefiedFeature){
+		Feature feature = classiefiedFeature.getFeature();
+		EList<Constraint> constraintsFeature = feature.getConstraints();
+		System.out.println("terms.get(0): "+terms.get(0).toString());
+		System.out.println(constraintsFeature);
+		System.out.println(controller.getConstraints());
+		classiefiedFeatures.add(classiefiedFeature);
+		return true;  //change
 	}
 }
--- org.backend.storage/src/org/backend/storage/StorageController.java
+++ org.backend.storage/src/org/backend/storage/StorageController.java
@@ -21,6 +21,10 @@ import org.featuremapper.models.feature.Constraint;
 import org.featuremapper.models.feature.FeatureModel;
 import org.featuremapper.models.feature.FeaturePackage;
 
+import org.west.es.context.contextModel.Container;
+import org.west.es.context.contextModel.ContextInstance;
+import org.west.es.context.contextModel.ContextModelFactory;
+
 // TODO: Auto-generated Javadoc
 /**
  * The Class StorageController.
@@ -35,6 +39,10 @@ public class StorageController {
 	
 	/** The constraints. */
 	private List<Constraint> constraints;
+	
+	private ContextModelFactory contextFac;
+	
+	private Container contextContainer;
 	
 	/**
 	 * Instantiates a new storage controller.
@@ -62,88 +70,11 @@ public class StorageController {
 	private void init(){
 		setTerms(TextExpressionParser.parseExpressions(featuremodel));
 		setConstraints(featuremodel.getConstraints());
+		contextContainer = contextFac.createContainer();
 	}
 	
 	
-	//begin loadModel Code from /org.feature.model.slicer/test/formulaSlicer/formula/TestCNFBuild.java
-	 
-	/**
-	 * Load model.
-	 *
-	 * @param ePackage the e package
-	 * @param path the path
-	 * @param resourceSet the resource set
-	 * @return the e object
-	 */
-	private EObject loadModel(EPackage ePackage, String path,
-			ResourceSet resourceSet) {
-		initEMF(ePackage);
-
-		return loadModel(createFileURI(path, true), resourceSet);
-	}
-
-	/**
-	 * Load model.
-	 *
-	 * @param uri the uri
-	 * @param resourceSet the resource set
-	 * @return the e object
-	 */
-	private EObject loadModel(URI uri, ResourceSet resourceSet) {
-		// Obtain a new resource set if necessary
-		if (resourceSet == null)
-			resourceSet = new ResourceSetImpl();
-
-		// Get the resource
-		Resource resource = resourceSet.getResource(uri, true);
-
-		// Add adapter for reverse navigation along unidirectional links
-		ECrossReferenceAdapter adapter = ECrossReferenceAdapter
-				.getCrossReferenceAdapter(resourceSet);
-		if (adapter == null)
-			resourceSet.eAdapters().add(new ECrossReferenceAdapter());
-
-		// Return root model element
-		return resource.getContents().get(0);
-	}
-
-	/**
-	 * Creates the file uri.
-	 *
-	 * @param path the path
-	 * @param mustExist the must exist
-	 * @return the uri
-	 */
-	private URI createFileURI(String path, boolean mustExist) {
-		File filePath = new File(path);
-		if (!filePath.exists() && mustExist)
-			throw new IllegalArgumentException(path + " does not exist.");
-
-		return URI.createFileURI(filePath.getAbsolutePath());
-	}
-
-	/**
-	 * Register xmi factory as default.
-	 */
-	private void registerXMIFactoryAsDefault() {
-		// Add XMI factory to registry
-		Resource.Factory.Registry reg = Resource.Factory.Registry.INSTANCE;
-		Map<String, Object> m = reg.getExtensionToFactoryMap();
-		m.put("*", new XMIResourceFactoryImpl());
-	}
-
-	/**
-	 * Inits the emf.
-	 *
-	 * @param ePackage the e package
-	 */
-	private void initEMF(EPackage ePackage) {
-		// Initialize the model
-		// logger.debug("Initializing " + ePackage.getName());
-
-		ePackage.getName();
-		registerXMIFactoryAsDefault();
-	}
+	
 
 	/**
 	 * Gets the terms.
@@ -181,6 +112,90 @@ public class StorageController {
 		this.constraints = constraints;
 	}
 	
+	public FeatureModel getFeatureModel(){
+		return featuremodel;
+	}
+	
+	public ContextInstance getcontextContainer(){
+		ContextInstance instTmp = contextFac.createContextInstance();
+		contextContainer.getContextInstances().add(instTmp);
+		return instTmp;
+	}
+	//begin loadModel Code from /org.feature.model.slicer/test/formulaSlicer/formula/TestCNFBuild.java
+	 
+	/**
+	 * Load model.
+	 *
+	 * @param ePackage the e package
+	 * @param path the path
+	 * @param resourceSet the resource set
+	 * @return the e object
+	 */
+	private EObject loadModel(EPackage ePackage, String path,
+			ResourceSet resourceSet) {
+		initEMF(ePackage);
+
+		return loadModel(createFileURI(path, true), resourceSet);
+	}
+
+	/**
+	 * Load model.
+	 *
+	 * @param uri the uri
+	 * @param resourceSet the resource set
+	 * @return the e object
+	 */
+	private EObject loadModel(URI uri, ResourceSet resourceSet) {
+		// Obtain a new resource set if necessary
+		if (resourceSet == null)
+			resourceSet = new ResourceSetImpl();
+
+		// Get the resource
+		Resource resource = resourceSet.getResource(uri, true);
+
+		// Add adapter for reverse navigation along unidirectional links
+		ECrossReferenceAdapter adapter = ECrossReferenceAdapter
+			.getCrossReferenceAdapter(resourceSet);
+		if (adapter == null)
+			resourceSet.eAdapters().add(new ECrossReferenceAdapter());
+		// Return root model element
+			return resource.getContents().get(0);
+		}
+	/**
+	 * Creates the file uri.
+	 *
+	 * @param path the path
+	 * @param mustExist the must exist
+	 * @return the uri
+	 */
+	private URI createFileURI(String path, boolean mustExist) {
+		File filePath = new File(path);
+		if (!filePath.exists() && mustExist)
+			throw new IllegalArgumentException(path + " does not exist.");
+		return URI.createFileURI(filePath.getAbsolutePath());
+	}
+
+	/**
+	 * Register xmi factory as default.
+	 */
+	private void registerXMIFactoryAsDefault() {
+		// Add XMI factory to registry
+		Resource.Factory.Registry reg = Resource.Factory.Registry.INSTANCE;
+		Map<String, Object> m = reg.getExtensionToFactoryMap();
+		m.put("*", new XMIResourceFactoryImpl());
+	}
+
+	/**
+	 * Inits the emf.
+	 *
+	 * @param ePackage the e package
+	 */
+	private void initEMF(EPackage ePackage) {
+		// Initialize the model
+		// logger.debug("Initializing " + ePackage.getName());
+		ePackage.getName();
+		registerXMIFactoryAsDefault();
+	}	
 	//end loadModel Code from /org.feature.model.slicer/test/formulaSlicer/formula/TestCNFBuild.java
 	
 }
