d2c5e5ad7f7ef5aca6499b98d2c72b6e35f3cdd0
dath <dath@dath.info>
2012-12-04 22:06:28 +0100

    deleted old project-directories

--- bp.contexteditor.diagram/src/metamodel/diagram/edit/helpers/ContextEditHelper.java
+++
@@ -1,7 +0,0 @@
-package metamodel.diagram.edit.helpers;
-
-/**
- * @generated
- */
-public class ContextEditHelper extends MetamodelBaseEditHelper {
-}
--- bp.contexteditor.diagram/src/metamodel/diagram/edit/helpers/RuntimeConfigEditHelper.java
+++
@@ -1,7 +0,0 @@
-package metamodel.diagram.edit.helpers;
-
-/**
- * @generated
- */
-public class RuntimeConfigEditHelper extends MetamodelBaseEditHelper {
-}
