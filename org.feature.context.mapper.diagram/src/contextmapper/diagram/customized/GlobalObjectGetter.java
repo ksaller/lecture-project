d72ab572004f88536184dac40a18eb20248165b6
Melvil <patpp17@web.de>
2012-12-04 19:36:22 +0100

    global object getter

---
+++ org.feature.context.mapper.diagram/src/contextmapper/diagram/customized/GlobalObjectGetter.java
@@ -0,0 +1,52 @@
+package contextmapper.diagram.customized;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.util.ECrossReferenceAdapter;
+import org.eclipse.gmf.runtime.diagram.ui.parts.DiagramEditor;
+import org.eclipse.ui.IWorkbench;
+
+import contextmapper.ContextDiagram;
+import contextmapper.diagram.part.ContextmapperDiagramEditorPlugin;
+
+/**
+* Leicher Zugriff auf wichtige Objekte
+*
+* @author Melvil
+*
+*/
+public class GlobalObjectGetter {
+
+	public static ContextDiagram getContextDiagram(){
+		return (ContextDiagram)getDiagramEditor().getDiagram().getElement().eResource().getContents().get(0);
+	}
+
+	public static IWorkbench getWorkbench(){
+		return ContextmapperDiagramEditorPlugin.getInstance().getWorkbench();
+	}
+
+	public static DiagramEditor getDiagramEditor(){
+		return (DiagramEditor)getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
+	}
+
+	public static EObject loadModel(URI uri, ResourceSet resourceSet) {
+		// Obtain a new resource set if necessary
+		if (resourceSet == null)
+			resourceSet = new ResourceSetImpl();
+
+		// Get the resource
+		Resource resource = resourceSet.getResource(uri, true);
+
+		// Add adapter for reverse navigation along unidirectional links
+		ECrossReferenceAdapter adapter = ECrossReferenceAdapter
+				.getCrossReferenceAdapter(resourceSet);
+		if (adapter == null)
+			resourceSet.eAdapters().add(new ECrossReferenceAdapter());
+
+		// Return root model element
+		return resource.getContents().get(0);
+	}
+}
